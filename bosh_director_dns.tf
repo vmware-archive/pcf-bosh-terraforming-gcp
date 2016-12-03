d778ce7fce3e43f785aef5f4ceb1c9dd713fd1e9
Dan Wendorf <dwendorf@pivotal.io>
2016-12-02 16:13:27 -0800

    Director DNS should be director.* instead of bosh.*

--- bosh_director_dns.tf
+++ bosh_director_dns.tf
@@ -3,7 +3,7 @@ resource "google_compute_address" "bosh-director" {
 }
 
 resource "google_dns_record_set" "bosh-director-dns" {
-  name       = "bosh.${google_dns_managed_zone.env_dns_zone.dns_name}"
+  name       = "director.${google_dns_managed_zone.env_dns_zone.dns_name}"
   type       = "A"
   ttl        = 300
 
